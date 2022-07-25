376b82d433a445f7f414528b6d5cf628a0ca5c26
MASV22 <miguea74@gmail.com>
2022-07-25 16:03:15 -0500

    feature: limpieza codigo

---
+++ setpuntajesdata.php
@@ -0,0 +1,78 @@
+<?php 
+//(ESTABLECER PUNTAJES EN 0)
+
+header("Access-Control-Allow-Origin: *");
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Crear Cliente---------------------------------------------------------------------
+
+$client = new MongoDB\Client('mongodb+srv://miguel:22699@cluster0.amgor.mongodb.net/?retryWrites=true&w=majority');
+
+// Traer Base de datos---------------------------------------------------------------
+
+$database = $client->MultimediaS;
+
+// Crear o Traer coleccion-----------------------------------------------------------
+
+$collection = $database->entrenamiento;
+$collection2 = $database->puntaje;
+
+$datoUsuarioid = $_GET['userid'];
+
+$usuarioid = new MongoDB\BSON\ObjectId($datoUsuarioid);
+
+$Datos = $collection->find();
+
+
+if($Datos != null){
+
+    $arreglo = array();
+    $arreglofinal = array();
+
+    $filtro = ['userID' => $usuarioid];
+
+   foreach($Datos as $doc) 
+   {
+        /// Buscar datos id y establecer parametros id y title -----------------------------
+
+        $datosMundo = $doc->jsonSerialize();
+        array_push($arreglo, $datosMundo);  
+        $a =  $doc -> _id;
+        $b = $doc -> title;
+
+        /// Buscar por medio de entrenamientoID y Userid si existe en la tabla puntaje------------
+
+        $filtro2 = ['entrenamientoID' => $a];
+        $Busqueda = $collection2->findOne(array(
+            '$and' => array($filtro2,$filtro))
+        );
+        var_dump($Busqueda);
+
+        if ($Busqueda == NULL) 
+        {
+            print("agregó");
+            $collection2->insertOne(
+
+                [
+                    'userID' => new MongoDB\BSON\ObjectId($usuarioid),
+                    'entrenamientoID' => new MongoDB\BSON\ObjectId($a),
+                    'puntaje' => 0,
+                    'entrenamientoTitle' => $b,
+                ]
+    
+            );
+        }
+    }
+}
+else 
+{
+    $var = json_encode(null);
+    die($var);
+}
+
+
+
+?>
+
+
+
---
+++ updatepuntajesdata.php
@@ -0,0 +1,50 @@
+<?php 
+//(ACTUALIZAR PUNJATES)
+
+header("Access-Control-Allow-Origin: *");
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Crear Cliente---------------------------------------------------------------------
+
+$client = new MongoDB\Client('mongodb+srv://miguel:22699@cluster0.amgor.mongodb.net/?retryWrites=true&w=majority');
+
+// Traer Base de datos---------------------------------------------------------------
+
+$database = $client->MultimediaS;
+
+// Crear o Traer coleccion-----------------------------------------------------------
+
+$collection = $database->puntaje;
+
+//Variables que necesito ------------------------------------------------------------
+
+$usuarioid = $_GET['userid'];
+$trainingid = $_GET['trainingid'];
+$puntaje = $_GET['puntaje'];
+
+// Actualizar un dato ---------------------------------------------------------------
+
+$filtro = ['userID' => new MongoDB\BSON\ObjectId($usuarioid)];
+$filtro2 = ['entrenamientoID' => new MongoDB\BSON\ObjectId($trainingid)];
+$Busqueda = $collection->findOne(array(
+    '$and' => array($filtro,$filtro2))
+);
+
+if($Busqueda != null)
+{
+    $datosMundo = $Busqueda->jsonSerialize();  
+    $var = json_encode($datosMundo); 
+    print($var);     
+         
+    $update = ['$set' => ['puntaje' => $puntaje]];
+    $Actualizar = $collection->updateOne($datosMundo,$update);     
+}
+else{
+    print("NO");
+}
+
+
+?>
+
+
+
