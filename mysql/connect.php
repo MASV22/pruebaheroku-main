4c80baca44ec1a05e7af86533e3b5a66ba2b61c1
MASV22 <miguea74@gmail.com>
2022-09-01 10:05:09 -0500

    connect

--- mysql/connect.php
+++ mysql/connect.php
@@ -1,16 +1,29 @@
 <?php 
     //  (COMPARAR MYSQL)
     header("Access-Control-Allow-Origin: *");
-    $dbname = "pruebasu_pwa";
-    $dns = "mysql:host=localhost;dbname=$dbname";
+    // $dbname = "pruebasu_pwa";
+    // $dns = "mysql:host=localhost;dbname=$dbname";
+    // $user = "pruebasu_pwa1";
+    // $pass = "Admin22699";
+    // try{
+    //     $db = new PDO($dns,$user,$pass);
+    // }catch(PDOException $e){
+    //     echo $e->getMessage();
+    // }
+    
+    $servername = "localhost";
     $user = "pruebasu_pwa1";
     $pass = "Admin22699";
-    try{
-        $db = new PDO($dns,$user,$pass);
-    }catch(PDOException $e){
-        echo $e->getMessage();
+    $dbname = "pruebasu_pwa";
+
+    $conn = mysqli_connect($servername,$user,$pass,$dbname);
+
+    if($conn){
+        echo "Connexion establecida";
     }
- 
+    else{
+        echo "error";
+    }
 ?>
 
 
